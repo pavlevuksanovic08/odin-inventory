f92bae664dacdcedcf303a319fe6c43b1a974b78
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-04-29 23:18:32 +0200

    feat: create initial databse insertion

---
+++ db/populatedb.js
@@ -0,0 +1,77 @@
+require("dotenv").config()
+const { Client } = require("pg")
+
+const SQL = `
+
+    create table director (
+        directorID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        fullName varchar(60)
+    );
+
+    create table movie (
+        movieID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        title varchar(50) NOT NULL,
+        directorID INTEGER,
+        FOREIGN KEY (directorID) REFERENCES director(directorID)
+    );
+
+    create table genre (
+        genreID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name varchar(30) NOT NULL UNIQUE
+    );
+
+    create table movie_genre(
+        movieID INTEGER,
+        genreID INTEGER,
+        PRIMARY KEY (movieID, genreID),
+        FOREIGN KEY (movieID) REFERENCES movie(movieID),
+        FOREIGN KEY (genreID) REFERENCES genre(genreID)
+    );
+
+insert into director (fullName) values 
+('Christopher Nolan'),
+('Quentin Tarantino'),
+('Martin Scorsese');
+
+insert into movie (title, directorID) values
+('Inception', 1),
+('Pulp Fiction', 2),
+('The Wolf of Wall Street', 3),
+('Django Unchained', 2);
+
+insert into genre (name) values
+('Action'),
+('Drama'),
+('Crime'),
+('Sci-Fi');
+
+insert into movie_genre (movieID, genreID) values
+-- Inception
+(1, 1),
+(1, 4),
+
+-- Pulp Fiction
+(2, 2),
+(2, 3),
+
+-- Wolf of Wall Street
+(3, 2),
+(3, 3),
+
+-- Django
+(4, 1),
+(4, 2);
+`
+
+async function main() {
+    console.log("seeding...")
+    const client = new Client({
+        connectionString: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+    })
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("Done.")
+}
+
+main()
