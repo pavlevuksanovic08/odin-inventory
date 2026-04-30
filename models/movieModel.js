fded7faccc00bdebe1a69f6132ead8ba69b17155
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-04-30 08:26:11 +0200

    feat: create model to get movies with same genre

--- models/movieModel.js
+++ models/movieModel.js
@@ -5,11 +5,30 @@ exports.getAllMovies = async () => {
     const { rows } = await pool.query(`
             select m.movieID, title, d.fullName as director, array_agg(g.name) as genre
             from movie m 
-            join director d 
+            join director d
             on m.directorid = d.directorid
             join movie_genre mg ON mg.movieid = m.movieid
             join genre g on g.genreid = mg.genreid
             group by m.movieID, d.fullname;
         `)
+    return rows;
+}
+
+exports.getMoviesByGenre = async (id) => {
+    const { rows } = await pool.query(`
+            select m.movieID, title, d.fullName as director, array_agg(g.name) as genre
+            from movie m 
+            join director d
+            on m.directorid = d.directorid
+            join movie_genre mg ON mg.movieid = m.movieid
+            join genre g on g.genreid = mg.genreid
+            where exists (
+                select *
+                from movie_genre mg2
+                where mg2.movieid = m.movieid
+                and mg2.genreid = $1
+            )
+            group by m.movieID, d.fullname;
+        `, [id])
     return rows;
 }
