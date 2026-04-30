a94e91db13a33c4942b0bf6cc01a84f24a679f22
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-04-30 08:27:02 +0200

    feat: add logic to show movies of selected genre and to stay on selected after render

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,15 @@ const movieModel = require("../models/movieModel")
 const genreModel = require("../models/genreModel")
 
 exports.indexGet = async (req, res) => {
-    const movies = await movieModel.getAllMovies();
-    const genres = await genreModel.getAllGenres();
-    res.render("index", { movies: movies, genres: genres })
+    const genreId = req.query.genre;
+    let movies;
+    let genres = await genreModel.getAllGenres();
+    if (genreId == 0) {
+        movies = await movieModel.getAllMovies();
+        
+    } else {
+        movies = await movieModel.getMoviesByGenre(genreId);
+    }
+    
+    res.render("index", { movies: movies, genres: genres, selected: genreId })
 }
