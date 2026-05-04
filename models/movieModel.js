9057d2da5fc51cd511739bad52efb4175f1ad5c9
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-04 21:05:54 +0200

    feat: create update movie model

--- models/movieModel.js
+++ models/movieModel.js
@@ -24,7 +24,7 @@ exports.getMovieById = async (id) => {
             on m.movieid = mg.movieid
             join genre g
             on mg.genreid = g.genreid
-            where m.movieid = ($1)
+            where m.movieid = $1
             group by m.movieid, d.fullname
         `, [id])
     return rows[0];
@@ -75,4 +75,52 @@ exports.deleteMovie = async (mId) => {
             delete from movie
             where movieid = $1;
         `, [mId])
+}
+
+exports.updateMovie = async (mId, updated) => {
+    
+    const client = await pool.connect();
+
+    try {
+        await client.query("BEGIN");
+
+        await client.query(`
+            WITH d AS (
+                INSERT INTO director (fullname)
+                VALUES ($1)
+                ON CONFLICT (fullname)
+                DO UPDATE SET fullname = EXCLUDED.fullname
+                RETURNING directorid
+            )
+            UPDATE movie
+            SET title = $2,
+                directorid = (SELECT directorid FROM d)
+            WHERE movieid = $3;
+        `, [updated.director, updated.title, mId]);
+
+        await client.query(`
+            DELETE FROM movie_genre WHERE movieid = $1;
+        `, [mId]);
+
+        await client.query(`
+            INSERT INTO movie_genre (movieid, genreid)
+            SELECT $1, UNNEST($2::int[])
+            ON CONFLICT DO NOTHING;
+        `, [mId, updated.genres]);
+
+        await client.query(`
+            DELETE FROM director d
+            WHERE NOT EXISTS (
+                SELECT 1 FROM movie m WHERE m.directorid = d.directorid
+            );
+        `);
+
+        await client.query("COMMIT")
+    } catch (err) {
+        await client.query("ROLLBACK");
+        throw err;
+    } finally {
+        client.release();
+    }
+
 }
